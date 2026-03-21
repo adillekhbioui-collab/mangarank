bc403eae3529c3d1e7bcfdd24beb48bcd1a28fd9
adil <adil.lekhb@gmail.com>
2026-03-21 18:04:29 +0000

    Configure CORS for deployment

--- backend/main.py
+++ backend/main.py
@@ -56,7 +56,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["http://localhost:3000/", "https://mannhwa-rank.vercel.app/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
